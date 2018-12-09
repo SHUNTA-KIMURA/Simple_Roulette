a8c7fb674dcb8e0fa6c35b7f8b899a7d9025a732
SHUNTA-KIMURA <ks2017mssotu88@icloud.com>
2018-12-09 14:34:24 +0900

    スタートボタンとストップボタンのvisible,goneのコードを書いた

--- app/src/main/java/android/lifeistech/com/simpleroulette/RouletteActivity.java
+++ app/src/main/java/android/lifeistech/com/simpleroulette/RouletteActivity.java
@@ -2,12 +2,25 @@ package android.lifeistech.com.simpleroulette;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RouletteActivity extends AppCompatActivity {
+    Button startButton,stopButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roulette);
+        stopButton=(Button)findViewById(R.id.stopButton);
+        startButton=(Button)findViewById(R.id.startButton);
+    }
+    public void start(View v){
+        startButton.setVisibility(View.GONE);
+        stopButton.setVisibility(View.VISIBLE);
+    }
+    public void stop(View v){
+        stopButton.setVisibility(View.GONE);
+        startButton.setVisibility(View.VISIBLE);
     }
 }
