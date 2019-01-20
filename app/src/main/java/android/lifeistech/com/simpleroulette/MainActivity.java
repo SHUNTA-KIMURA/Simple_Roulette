bfcc0142411e253f443f7b7a7b1bdbfa60fb30ef
SHUNTA-KIMURA <ks2017mssotu88@icloud.com>
2019-01-20 14:22:50 +0900

    ルーレットの機能を増やした

--- app/src/main/java/android/lifeistech/com/simpleroulette/MainActivity.java
+++ app/src/main/java/android/lifeistech/com/simpleroulette/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void next(View v){
+        editText=(EditText)findViewById(R.id.editText);
+        text=editText.getText().toString();
+        int maxCount=Integer.parseInt(text);
         Intent intent = new Intent(getApplication(), RouletteActivity.class);
+        intent.putExtra("number",maxCount);
         startActivity(intent);
     }
 }
--- app/src/main/java/android/lifeistech/com/simpleroulette/RouletteActivity.java
+++ app/src/main/java/android/lifeistech/com/simpleroulette/RouletteActivity.java
@@ -1,26 +1,94 @@
 package android.lifeistech.com.simpleroulette;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RouletteActivity extends AppCompatActivity {
-    Button startButton,stopButton;
+    Button startButton, stopButton;
+    int maxCount;
+    PieChart mPieChart;
+    int count = 12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roulette);
-        stopButton=(Button)findViewById(R.id.stopButton);
-        startButton=(Button)findViewById(R.id.startButton);
+        stopButton = (Button) findViewById(R.id.stopButton);
+        startButton = (Button) findViewById(R.id.startButton);
+
+        Intent intent = getIntent();
+        maxCount = intent.getIntExtra("number", maxCount);
+        mPieChart = (PieChart) findViewById(R.id.pie_chart);
+        setupPieChartView();
     }
-    public void start(View v){
+
+    public void start(View v) {
+        mPieChart.spin(30000, mPieChart.getRotationAngle(), mPieChart.getRotationAngle() + 18000, Easing.EasingOption.EaseOutQuart);
         startButton.setVisibility(View.GONE);
         stopButton.setVisibility(View.VISIBLE);
     }
-    public void stop(View v){
+
+    public void stop(View v) {
+        mPieChart.spin(5000, mPieChart.getRotationAngle(), mPieChart.getRotationAngle() + 1800, Easing.EasingOption.EaseOutQuart);
         stopButton.setVisibility(View.GONE);
         startButton.setVisibility(View.VISIBLE);
     }
+
+
+
+
+
+    private void setupPieChartView() {
+        mPieChart.setUsePercentValues(true);
+
+        Legend legend = mPieChart.getLegend();
+        legend.setPosition(Legend.LegendPosition.BELOW_CHART_LEFT);
+
+        List<Float> values = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            values.add(100f);
+        }
+
+        List<Entry> entries = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            entries.add(new Entry(values.get(i), i));
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, "");
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        dataSet.setDrawValues(false);
+
+        List<String> labels = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            labels.add(i + 1 + "");
+        }
+
+        PieData pieData = new PieData(labels, dataSet);
+        pieData.setValueFormatter(new PercentFormatter());
+        pieData.setValueTextSize(20f);
+        pieData.setValueTextColor(Color.BLACK);
+
+        mPieChart.setData(pieData);
+
+    }
+
+
 }
